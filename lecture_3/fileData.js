08638a9851d014b1c5c192fa6ad71fa7c1a75ba0
Chris Shi <shishuangw@gmail.com>
2018-09-09 22:55:22 -0400

    fix async func in lab3.fileData.js

--- lecture_3/fileData.js
+++ lecture_3/fileData.js
@@ -6,34 +6,39 @@ const getFileAsString = async (path)=>{
     if(!path || typeof path != 'string'){
         throw "You must provide a path!";
     }
-    const data = await fs.readFileAsync(path,'utf-8');
-    return data;
+    return await fs.readFileAsync(path,'utf-8');
+
 };
 
 const getFileAsJSON = async (path) => {
-    return getFileAsString(path)
-        .then((data)=>{
-            return JSON.parse(data);
-        })
-        .catch((err)=>{
-            throw err;
-        })
+    try{
+        let data = await getFileAsString(path);
+        return JSON.parse(data)
+    }catch(err){
+        throw err;
+    }
 }
 
 const saveStringToFile = async(path, text) =>{
     if (!path || typeof path !== 'string')
 		throw "You must provide a path!";
 	if (typeof text !== 'string')
-		throw "You must provide text to write as a string!";
-
-	await fs.writeFileAsync(path, text);
+        throw "You must provide text to write as a string!";
+        
+    await fs.writeFileAsync(path, text);
 	return true;
+	
 }
 
 const saveJSONToFile = async(path, obj) => {
     if (!obj || typeof obj !== 'object')
         throw "You must provide an object to write!";
-    return saveStringToFile(path, text = JSON.stringify(obj, null, 4));
+    try{
+        const text = JSON.stringify(obj, null, 4)
+        return saveStringToFile(path, text);
+    }catch(err){
+        throw err;
+    }
 }
 module.exports = {
 	getFileAsString: getFileAsString,
