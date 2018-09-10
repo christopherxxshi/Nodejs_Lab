db9379df8b7e2af56dfdfe3334dfbb30f336ef80
Chris Shi <shishuangw@gmail.com>
2018-09-09 21:49:18 -0400

    upgrade lec 3

--- lecture_3/app.js
+++ lecture_3/app.js
@@ -1,11 +1,14 @@
 const fileData = require("./fileData");
 const textMetrics = require("./textMetrics");
 const fs = require('fs');
+
 async function main() {
     for(let i = 1; i < 4; i++){
         const existFlag = fs.existsSync(`./chapter${i}.result.json`);
         if(existFlag){
             try{
+                console.log(`chapter${i}.result.json Exists!`);
+                console.log(`Reading chapter${i}.result.json`);
                 let JsonData = await fileData.getFileAsJSON(`./chapter${i}.result.json`);
                 console.log(JsonData);
             }catch(err){
@@ -13,8 +16,10 @@ async function main() {
             }
         }else{
             try{
+                console.log(`Reading chapter${i}.txt`);
                 let content = await fileData.getFileAsString(`./chapter${i}.txt`);
                 let result = textMetrics.createMetrics(content);
+                console.log(`Writing chapter${i}.result.json`);
                 await fileData.saveJSONToFile(`./chapter${i}.result.json`, result);
             }catch(err){
                 console.log(err);
--- lecture_3/fileData.js
+++ lecture_3/fileData.js
@@ -1,55 +1,39 @@
 const bluebird = require("bluebird");
 const Promise = bluebird.Promise;
-
 const fs = bluebird.promisifyAll(require("fs"));
 
-getFileAsString = async (path)=>{
+const getFileAsString = async (path)=>{
     if(!path || typeof path != 'string'){
         throw "You must provide a path!";
     }
-    const data = await fs.readFileAsync(path,'utf-8').catch((err)=>{
-        throw err;
-    })
+    const data = await fs.readFileAsync(path,'utf-8');
     return data;
 };
 
-getFileAsJSON = async (path) => {
+const getFileAsJSON = async (path) => {
     return getFileAsString(path)
         .then((data)=>{
-            try{
-                return JSON.parse(data);
-            }catch(err){
-                throw(err);
-            }
+            return JSON.parse(data);
         })
         .catch((err)=>{
             throw err;
         })
 }
 
-saveStringToFile = async(path, text) =>{
+const saveStringToFile = async(path, text) =>{
     if (!path || typeof path !== 'string')
 		throw "You must provide a path!";
-	
 	if (typeof text !== 'string')
 		throw "You must provide text to write as a string!";
 
-	await fs.writeFileAsync(path, text).catch((err)=>{
-        throw err;
-    });
+	await fs.writeFileAsync(path, text);
 	return true;
 }
 
-saveJSONToFile = async(path, obj) => {
+const saveJSONToFile = async(path, obj) => {
     if (!obj || typeof obj !== 'object')
         throw "You must provide an object to write!";
-    try{
-        const text = JSON.stringify(obj, null, 4);
-        return saveStringToFile(path, text);
-        
-    }catch(err){
-        throw err;
-    }
+    return saveStringToFile(path, text = JSON.stringify(obj, null, 4));
 }
 module.exports = {
 	getFileAsString: getFileAsString,
